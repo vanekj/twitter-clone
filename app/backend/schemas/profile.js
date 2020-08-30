203015f6cdc0016535a1a2ddd76ba26dd0db279c
vanekj <info@jakub-vanek.eu>
2020-08-30 13:17:31 +0200

    feat(backend): add profile id into schema

--- app/backend/schemas/profile.js
+++ app/backend/schemas/profile.js
@@ -1,6 +1,7 @@
 module.exports = (profile) => {
 	if (typeof profile === 'object' && profile) {
 		return {
+			id: profile.id,
 			firstName: profile.firstName,
 			lastName: profile.lastName,
 			fullName: profile.fullName,
