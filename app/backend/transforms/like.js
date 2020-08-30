c101510d7d6be17d98fc3d2df068514e17d65317
vanekj <info@jakub-vanek.eu>
2020-08-30 15:52:14 +0200

    feat(backend): add date into like transform

--- app/backend/transforms/like.js
+++ app/backend/transforms/like.js
@@ -4,7 +4,8 @@ module.exports = (like) => {
 	if (typeof like === 'object' && like) {
 		return {
 			id: like._id,
-			author: profileTransform(like.author)
+			author: profileTransform(like.author),
+			date: like.createdAt
 		};
 	} else {
 		return like;
