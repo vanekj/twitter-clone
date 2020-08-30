4f3a8ff6267c5b9d1b96e20a3c6949479c7f5d02
vanekj <info@jakub-vanek.eu>
2020-08-30 12:16:38 +0200

    fix(backend): return null if given profile is not an object

--- app/backend/schemas/profile.js
+++ app/backend/schemas/profile.js
@@ -1,6 +1,12 @@
-module.exports = (profile) => ({
-	firstName: profile.firstName,
-	lastName: profile.lastName,
-	fullName: `${profile.firstName} ${profile.lastName}`,
-	nickname: profile.nickname
-});
+module.exports = (profile) => {
+	if (typeof profile === 'object' && profile) {
+		return {
+			firstName: profile.firstName,
+			lastName: profile.lastName,
+			fullName: `${profile.firstName} ${profile.lastName}`,
+			nickname: profile.nickname
+		};
+	} else {
+		return null;
+	}
+};
