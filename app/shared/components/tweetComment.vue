48e4e787af68836ab43962cc1733bba9a9ae34cf
vanekj <info@jakub-vanek.eu>
2020-09-11 20:43:40 +0200

    feat(shared): remove link to comment id

--- app/shared/components/tweetComment.vue
+++ app/shared/components/tweetComment.vue
@@ -6,11 +6,9 @@
 					<strong class="text-body">{{ comment.author.fullName }}</strong>
 					<span class="text-muted">@{{ comment.author.username }}</span>
 				</n-link>
-				<n-link v-b-tooltip.hover.bottom class="text-body" :title="comment.createdAt | date()" :to="{ name: 't-id', params: { id: comment._id } }">
-					<small>
-						{{ comment.createdAt | date('simple') }}
-					</small>
-				</n-link>
+				<small v-b-tooltip.hover.bottom class="text-body" :title="comment.createdAt | date()">
+					{{ comment.createdAt | date('simple') }}
+				</small>
 				<b-dropdown v-if="userIsAuthor" class="ml-auto float-right" size="sm" variant="light" no-caret right>
 					<template v-slot:button-content>
 						<b-icon-three-dots-vertical />
